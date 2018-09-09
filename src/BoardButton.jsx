21e956c30c31884bdc26d886491caddf86add67a
ricglz0201 <rjgcastillo@hotmail.com>
2018-09-09 18:54:06 -0500

    Implement front-end of footer with buttons

---
+++ src/BoardButton.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class BoardButton extends Component {
+  constructor(props) {
+    super();
+    this.props = props;
+  }
+
+  render() {
+    return(
+      <div className="col-2">
+        <button 
+          className="btn btn-game btn-lg btn-danger"
+          onClick={this.props.func} 
+          >
+          <p>{this.props.text}</p>
+        </button>
+      </div>
+    );
+  }
+}
+
+export default BoardButton;
