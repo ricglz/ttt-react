6620a8e845c561923ffacdf977b5adffa795fce5
ricglz0201 <rjgcastillo@hotmail.com>
2018-09-20 16:57:59 -0500

    Added responsiveness

--- src/BigBoard.jsx
+++ src/BigBoard.jsx
@@ -9,8 +9,8 @@ class BigBoard extends Component {
 
   render() {
     return (
-      <div className="row justify-content-center">
-        <div className="col-6">
+      <div className="game row justify-content-center">
+        <div className="col-9 col-sm-8 col-md-7 col-lg-6 col-xl-6">
           <BoardRow
             rowNum={0}
             boardGame={this.props.boardGame}
