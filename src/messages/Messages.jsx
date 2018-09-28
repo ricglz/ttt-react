c400cb234ec3ed4da766ec661930ac13b1a0afb9
ricglz0201 <rjgcastillo@hotmail.com>
2018-09-27 19:00:50 -0500

    Add locales for Game Page

--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -1,18 +1,45 @@
 const Messages = {
   en: {
     "homePage.title": "Home Page",
-    "homePage.sp": "Single Player",
-    "homePage.mp": "Local Multiplayer"
+    "game.score": "Score",
+    "game.xScore": "X's score",
+    "game.oScore": "O's score",
+    "game.easy": "Easy",
+    "game.medium": "Medium",
+    "game.hard": "Hard",
+    "game.placeholder": "Choose the difficulty (Default easy)",
+    "game.reset": "Reset",
+    "shared.sp": "Single Player",
+    "shared.mp": "Local Multiplayer",
+    "shared.back": "Back"
   },
   es: {
     "homePage.title": "Página Principal",
-    "homePage.sp": "Un jugador",
-    "homePage.mp": "Multijugador local"
+    "game.score": "Puntuación",
+    "game.xScore": "Puntuación de X",
+    "game.oScore": "Puntuación de O",
+    "game.easy": "Fácil",
+    "game.medium": "Medio",
+    "game.hard": "Difícil",
+    "game.placeholder": "Escoge la dificultad (Por defecto fácil)",
+    "game.reset": "Rendirse",
+    "shared.sp": "Un jugador",
+    "shared.mp": "Multijugador local",
+    "shared.back": "Atrás"
   },
   fr: {
     "homePage.title": "Page Principale",
-    "homePage.sp": "Joueur unique",
-    "homePage.mp": "Local multijoueur"
+    "game.score": "Score",
+    "game.xScore": "Score de X",
+    "game.oScore": "Score d'O",
+    "game.easy": "Facile",
+    "game.medium": "Moyen",
+    "game.hard": "Difficile",
+    "game.placeholder": "Choisez-vous la difficulté (Par defaut facile)",
+    "game.reset": "Reset",
+    "shared.sp": "Joueur unique",
+    "shared.mp": "Local multijoueur",
+    "shared.back": "Revenir"
   }
 };
 
