37ed82f43dd29a41d962a5a5f6af8aeacb94ffb4
Jamil Hijjawi <j.hijjawi@gmail.com>
2018-10-28 12:47:55 +0200

    Add arabic localization

--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -2,12 +2,14 @@ import en from './en';
 import es from './es';
 import fr from './fr';
 import pt from './pt';
+import ar from './ar';
 
 const Messages = {
   en: en,
   es: es,
   fr: fr,
   pt: pt,
+  ar: ar
 };
 
 export default Messages;
