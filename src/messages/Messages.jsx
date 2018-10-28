8fd3f6da88f1f2716e3c95d3e7b182440a85b62f
Yamil Garcia Hernandez <yamil.garcia.h@gmail.com>
2018-10-28 11:34:31 -0400

    Ru Lang added

--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -2,12 +2,14 @@ import en from './en';
 import es from './es';
 import fr from './fr';
 import pt from './pt';
+import ru from './ru';
 
 const Messages = {
   en: en,
   es: es,
   fr: fr,
   pt: pt,
+  ru: ru,
 };
 
 export default Messages;
