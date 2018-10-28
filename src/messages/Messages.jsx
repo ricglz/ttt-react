73fbd6160f98cd0e863a83da0cf579b85cb4d6bc
yoking-human <godppkyh@gmail.com>
2018-10-28 16:10:35 +0900

    [UPDATE] Modify Messages.jsx

--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -2,12 +2,14 @@ import en from './en';
 import es from './es';
 import fr from './fr';
 import pt from './pt';
+import kr from './kr';
 
 const Messages = {
   en: en,
   es: es,
   fr: fr,
   pt: pt,
+  kr: kr
 };
 
 export default Messages;
