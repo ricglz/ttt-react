f70861e705c4fab72d1151b4f95e1beb6df57305
Tommy Kjær Andersen <tommy.andersen@idean.com>
2018-10-28 08:39:02 +0100

    Add danish to menu

--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -2,12 +2,14 @@ import en from './en';
 import es from './es';
 import fr from './fr';
 import pt from './pt';
+import da from './da';
 
 const Messages = {
   en: en,
   es: es,
   fr: fr,
   pt: pt,
+  da: da,
 };
 
 export default Messages;
