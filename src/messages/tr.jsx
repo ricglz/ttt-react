57dd125b2489f097bee312cb4b5f00fe99765af5
murat emir cabaroğlu <muratemir.19988@gmail.com>
2018-10-28 18:59:54 +0300

    Turkish file added

---
+++ src/messages/tr.jsx
@@ -0,0 +1,27 @@
+const tr = {
+  "homePage.title": "Ana Sayfa",
+  "game.score": "Skor",
+  "game.xScore": "X'in Skoru",
+  "game.oScore": "O'ın Skoru",
+  "game.easy": "Kolay",
+  "game.medium": "Orta",
+  "game.hard": "Zor",
+  "game.placeholder": "Zorluk seç(Varsayılan Kolay)",
+  "game.reset": "Yeniden Başlat",
+  "tutorial.first":
+    "Oyunun mekanikleri basit, Aktif olan hücrelerden herhangi bir tanesine basmalısın, Aktif hücreler beyaz arkaplana sahip olanlar",
+  "tutorial.second":
+    "Bu oyundaki amaç satırlarda veya sutunlarda üç sembolü diagonal Şekilde bir araya getirmek",
+  "tutorial.third":
+    "İlk hamleyi herzaman istediğin hücreden yada ızgaradan yapabilirsin, sonraki hamleleri bir önceki hamleye göre karar verebilirsin, örnek olarak:",
+  "tutorial.fourth":
+    "Bir önceki örnekte, X oyuncusu istediği hamleyi gerçekleştirebilir (İlk oyuncu herzaman X olur), o alt orta hücreyi seçti, sonrasında O oyuncusu alt orta hücreye hamle gerçekleştirmek zorunda.",
+  "tutorial.fifth":
+    "Önceki görüntüde, görüyoruzki sonraki hamle sol alt hücre olmak zorunda çünkü beyaz arkaplana sahip tek hücre,ve bir önceki hamle ortada yada sağ ust hücrede bulunuyordu çünkü sol alt ızgarada X yada O var.",
+  "shared.sp": "Tek Oyunculu",
+  "shared.mp": "Yerel Çok Oyunculu",
+  "shared.tutorial": "Nasıl oynanır",
+  "shared.back": "Geri"
+};
+
+export default tr;
