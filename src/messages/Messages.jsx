9c7c18c4c06ca2c8d68b1aca9a242b34a2a9edd9
Izanderson Florencio <izandersonf@gmail.com>
2018-10-27 21:59:44 -0300

    Translated to portuguese

--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -1,11 +1,13 @@
 import en from './en';
 import es from './es';
 import fr from './fr';
+import pt from './pt';
 
 const Messages = {
   en: en,
   es: es,
-  fr: fr
+  fr: fr,
+  pt: pt,
 };
 
 export default Messages;
