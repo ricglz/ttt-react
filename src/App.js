f51a2dc9f01c131717fe04b571272e051c5ab669
Stefan Ivic <nightwalkerkg@gmail.com>
2018-10-28 11:20:57 +0100

    Implements a Serbian translation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import en from "react-intl/locale-data/en";
 import fr from "react-intl/locale-data/fr";
 import es from "react-intl/locale-data/es";
 import pt from "react-intl/locale-data/pt";
+import sr from "react-intl/locale-data/sr";
 import Messages from "./messages/Messages";
 import Layout from "./Layout";
 
-addLocaleData([...en, ...fr, ...es, ...pt]);
+addLocaleData([...en, ...fr, ...es, ...pt, ...sr]);
 
 class App extends Component {
   constructor(props) {
--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -2,12 +2,14 @@ import en from './en';
 import es from './es';
 import fr from './fr';
 import pt from './pt';
+import sr from './sr';
 
 const Messages = {
   en: en,
   es: es,
   fr: fr,
   pt: pt,
+  sr: sr,
 };
 
 export default Messages;
