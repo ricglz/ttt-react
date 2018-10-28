cb63e4ccfd1fe5c038197870b791a1c2453fc238
murat emir cabaroğlu <muratemir.19988@gmail.com>
2018-10-28 19:02:21 +0300

    editted for Turkish lang

---
+++ src/app.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { addLocaleData, IntlProvider } from "react-intl";
+import en from "react-intl/locale-data/en";
+import fr from "react-intl/locale-data/fr";
+import es from "react-intl/locale-data/es";
+import pt from "react-intl/locale-data/pt";
+import tr from "react-intl/locale-data/tr";
+import Messages from "./messages/Messages";
+import Layout from "./Layout";
+
+addLocaleData([...en, ...fr, ...es, ...pt,...tr]);
+
+class App extends Component {
+  constructor(props) {
+    super();
+    this.props = props;
+    this.state = this.originalState();
+    this.changeLocale = this.changeLocale.bind(this);
+  }
+
+  changeLocale(newLocale) {
+    this.setState({
+      locale: newLocale
+    });
+  }
+
+  originalState() {
+    return {
+      locale: "en"
+    };
+  }
+  
+  render() {
+    return (
+      <IntlProvider
+        locale={this.state.locale}
+        messages={Messages[this.state.locale]}
+      >
+        <Layout changeLocale={this.changeLocale} locale={this.state.locale} />
+      </IntlProvider>
+    );
+  }
+}
+
+//
+export default App;
