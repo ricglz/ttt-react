8941a267c807da7302b7d4fe51e2f52e0f174df4
yoking-human <godppkyh@gmail.com>
2018-10-28 16:09:31 +0900

    [UPDATE] Modified App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import en from "react-intl/locale-data/en";
 import fr from "react-intl/locale-data/fr";
 import es from "react-intl/locale-data/es";
 import pt from "react-intl/locale-data/pt";
+import pt from "react-intl/locale-data/kr";
 import Messages from "./messages/Messages";
 import Layout from "./Layout";
 
-addLocaleData([...en, ...fr, ...es, ...pt]);
+addLocaleData([...en, ...fr, ...es, ...pt, ...kr]);
 
 class App extends Component {
   constructor(props) {
