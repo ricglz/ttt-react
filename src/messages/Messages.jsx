413c6482d984cec3efd63d541b6f77700fa9b2ef
Yamil Garcia Hernandez <yamil.garcia.h@gmail.com>
2018-10-28 11:42:23 -0400

    Swedish Lang

--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -2,12 +2,14 @@ import en from './en';
 import es from './es';
 import fr from './fr';
 import pt from './pt';
+import sv from './sv';
 
 const Messages = {
   en: en,
   es: es,
   fr: fr,
   pt: pt,
+  sv: sv,
 };
 
 export default Messages;
