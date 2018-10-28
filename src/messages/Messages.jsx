3aa2eacd276b93078e5cf3181b2173672fbbd5b4
Vivek Molkar <vivekmolkar@gmail.com>
2018-10-28 10:58:06 +0530

    Add hindi translation

--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -2,12 +2,14 @@ import en from './en';
 import es from './es';
 import fr from './fr';
 import pt from './pt';
+import hi from './hi';
 
 const Messages = {
   en: en,
   es: es,
   fr: fr,
   pt: pt,
+  hi: hi,
 };
 
 export default Messages;
