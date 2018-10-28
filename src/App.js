bcaaf99ad6b672c1a5dabebc2f94a3a9324b7a06
Jamil Hijjawi <j.hijjawi@gmail.com>
2018-10-28 12:48:23 +0200

    Add direction and util classes to support arabic locale

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import en from "react-intl/locale-data/en";
 import fr from "react-intl/locale-data/fr";
 import es from "react-intl/locale-data/es";
 import pt from "react-intl/locale-data/pt";
+import ar from "react-intl/locale-data/ar";
 import Messages from "./messages/Messages";
 import Layout from "./Layout";
 
-addLocaleData([...en, ...fr, ...es, ...pt]);
+addLocaleData([...en, ...fr, ...es, ...pt, ...ar]);
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +16,7 @@ class App extends Component {
     this.props = props;
     this.state = this.originalState();
     this.changeLocale = this.changeLocale.bind(this);
+    this.getDirection = this.getDirection.bind(this);
   }
 
   changeLocale(newLocale) {
@@ -28,14 +30,24 @@ class App extends Component {
       locale: "en"
     };
   }
-  
+
+  getDirection() {
+    return this.state.locale === 'ar' ? 'rtl' : 'ltr';
+  }
+
+  getLocaleClass() {
+    return this.state.locale === 'ar' ? 'text-right' : 'text-left';
+  }
+
   render() {
     return (
       <IntlProvider
         locale={this.state.locale}
         messages={Messages[this.state.locale]}
       >
-        <Layout changeLocale={this.changeLocale} locale={this.state.locale} />
+        <div dir={this.getDirection()} className={this.getLocaleClass()}>
+          <Layout changeLocale={this.changeLocale} locale={this.state.locale} />
+        </div>
       </IntlProvider>
     );
   }
