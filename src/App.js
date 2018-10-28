288eaa6529ed93c625fe9bef4c6c757952d3729e
igavelyuk <gavelyukworking@gmail.com>
2018-10-28 15:41:09 +0200

    added Ukrainian translation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import en from "react-intl/locale-data/en";
 import fr from "react-intl/locale-data/fr";
 import es from "react-intl/locale-data/es";
 import pt from "react-intl/locale-data/pt";
+import pt from "react-intl/locale-data/uk";
 import Messages from "./messages/Messages";
 import Layout from "./Layout";
 
-addLocaleData([...en, ...fr, ...es, ...pt]);
+addLocaleData([...en, ...fr, ...es, ...pt, ...uk]);
 
 class App extends Component {
   constructor(props) {
--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -2,12 +2,14 @@ import en from './en';
 import es from './es';
 import fr from './fr';
 import pt from './pt';
+import uk from './uk';
 
 const Messages = {
   en: en,
   es: es,
   fr: fr,
   pt: pt,
+  uk: uk,
 };
 
 export default Messages;
