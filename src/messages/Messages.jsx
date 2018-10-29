47c188aea2486b7b9e128426bfb658e4209b88c9
Pau Rosello <pau.rosello.sngular@xbyorange.com>
2018-10-29 11:09:40 +0100

    Catalan translation

--- src/messages/Messages.jsx
+++ src/messages/Messages.jsx
@@ -15,10 +15,11 @@ import sr from './sr';
 import sv from './sv';
 import uk from './uk';
 import zh from './zh';
+import ca from './ca';
 
 const Messages = {
   en,  es,  fr,  pt, de, it, hi, mr, ko, ja, da, sr,
-  id, uk, ru, sv, zh
+  id, uk, ru, sv, zh, ca
 }
 
 export default Messages;
---
+++ src/messages/ca.jsx
@@ -0,0 +1,27 @@
+const ca = {
+  "homePage.title": "Pàgina Principal",
+  "game.score": "Puntuació",
+  "game.xScore": "Puntuació de X",
+  "game.oScore": "Puntuació de O",
+  "game.easy": "Fàcil",
+  "game.medium": "Mitjà",
+  "game.hard": "Difícil",
+  "game.placeholder": "Trieu la dificultat (Per defecto fàcil)",
+  "game.reset": "Abandonar",
+  "tutorial.first":
+    "Les mecàniqiues del joc son simples, has de clicar en cualsevol de les cel·les, de cualsevol matriu disponible (aquelles que tenen el fons blanc)",
+  "tutorial.second":
+    "L'objectiu d'aquest jos es tenir tres X u O juntes, be en diagonal, columna o fila.",
+  "tutorial.third":
+    "El primer moviment sempre es aleatori, sense importa la matriu o cel·la. Observa l'exemple:",
+  "tutorial.fourth":
+    "En l'exemple anterior es podia moure on es vulgui, cal destacar que el primer jugador serà la X. En aquest cas ha escollit la cel·la de baix al mig. En consecuencia, el jugador 0 ha de fer el se moviment a la matriu inferior.",
+  "tutorial.fifth":
+    "En la imatge anterior podem veure que el seguent moviment te que ser a la cel·la inferior esquerra, això es deu a que es la unica cel·la amb fons blan. Es pot saber que l'últim moviment ha sigut a la cel·la central o superiro esquerra  perque son les uniques que tenen la cel·la inferior esquerra ocupades.",
+  "shared.sp": "Un jugador",
+  "shared.mp": "Multijugador local",
+  "shared.tutorial": "Cóm jugar",
+  "shared.back": "Enrrere"
+};
+
+export default ca;
