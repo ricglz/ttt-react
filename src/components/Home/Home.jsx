775b8ab19967ef52bd67bc2f6f8a6b6feab35589
Ricardo González Castillo <rjgcastillo@hotmail.com>
2018-12-10 19:39:08 -0600

    Add contributors at frontpage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Octokat from 'octokat';
 import { FormattedMessage } from 'react-intl';
 import HomeButton from './HomeButton';
+import Contributor from './Contributor';
+import '../../css/contributor.css';
 
-const Home = ({ changeToAi, changeToPvp, changeToTutorial }) => (
-  <div className="container text-center">
-    <div className="row">
-      <div className="col">
-        <FormattedMessage id="homePage.title" default="Home Page">
-          {txt => <h1>{txt}</h1>}
-        </FormattedMessage>
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { contributors: null };
+  }
+
+  componentDidMount() {
+    const octo = new Octokat();
+    octo.repos('ricglz0201', 'ttt-react').contributors.fetch()
+      .then(({ items }) => {
+        const contributors = items.map(({ avatarUrl, htmlUrl, login }) => (
+          <Contributor
+            avatarUrl={avatarUrl}
+            htmlUrl={htmlUrl}
+            login={login}
+          />
+        ));
+        this.setState({ contributors });
+      });
+  }
+
+  render() {
+    const { changeToAi, changeToPvp, changeToTutorial } = this.props;
+    const { contributors } = this.state;
+    return (
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">
+            <FormattedMessage id="homePage.title" default="Home Page">
+              {txt => <h1>{txt}</h1>}
+            </FormattedMessage>
+          </div>
+        </div>
+        <HomeButton func={changeToAi} text="shared.sp" />
+        <HomeButton func={changeToPvp} text="shared.mp" />
+        <HomeButton func={changeToTutorial} text="shared.tutorial" />
+        <ul className="contributors">
+          {contributors}
+        </ul>
       </div>
-    </div>
-    <HomeButton func={changeToAi} text="shared.sp" />
-    <HomeButton func={changeToPvp} text="shared.mp" />
-    <HomeButton func={changeToTutorial} text="shared.tutorial" />
-  </div>
-);
+    );
+  }
+}
 
 Home.propTypes = {
   changeToTutorial: PropTypes.func.isRequired,
