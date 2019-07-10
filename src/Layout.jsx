1c3341db7531ab68dd342151c0544131b5347847
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-07-09 22:38:23 -0700

    Fix some bugs from codebeat errors

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -11,6 +11,7 @@ import GameMenu from './components/OnlineGame/GameMenu';
 import Tutorial from './components/Tutorial/Tutorial';
 import LanguageFooter from './components/Layout/LanguageFooter';
 import LanguagePage from './components/Languages/LanguagePage';
+import { userPropType } from './constants/props';
 import './css/bootstrap.css';
 import './css/home.css';
 import './css/board.css';
@@ -20,46 +21,25 @@ import { getRedirect } from './firebase/firebase';
 
 const renderSinglePlayer = () => <Game ai />;
 
-function Layout({ locale, changeLocale }) {
-  const cachedUser = localStorage.getItem('user');
-  const initialUser = cachedUser ? JSON.parse(cachedUser) : {};
-  const [user, setUser] = useState(initialUser);
+function updateUser(setUser) {
+  getRedirect().then((response) => {
+    const tempUser = response.user;
+    if (tempUser) {
+      const {
+        displayName, email, phoneNumber, photoUrl, uid,
+      } = tempUser;
+      const newUser = {
+        name: displayName, email, phoneNumber, photoUrl, uid,
+      };
+      setUser(newUser);
+      localStorage.setItem('user', JSON.stringify(newUser));
+    }
+  });
+}
 
-  useEffect(() => {
-    getRedirect().then((response) => {
-      const tempUser = response.user;
-      if (tempUser) {
-        const {
-          displayName, email, phoneNumber, photoUrl, uid,
-        } = tempUser;
-        const newUser = {
-          name: displayName, email, phoneNumber, photoUrl, uid,
-        };
-        setUser(newUser);
-        localStorage.setItem('user', JSON.stringify(newUser));
-      }
-    });
-  }, [setUser]);
-
-  const logOut = useCallback(() => {
-    setUser({});
-    localStorage.removeItem('user');
-  }, [setUser]);
-
-  const renderGameMenu = React.useCallback(({ history }) => (
-    <GameMenu
-      logOut={logOut}
-      user={user}
-      history={history}
-    />
-  ), [logOut, user]);
-  const renderLanguage = useCallback(({ history }) => (
-    <LanguagePage
-      locale={locale}
-      changeLocale={changeLocale}
-      history={history}
-    />
-  ), [changeLocale, locale]);
+function renderLayout({
+  user, renderGameMenu, renderLanguage, locale,
+}) {
   return (
     <>
       <Router>
@@ -93,6 +73,46 @@ function Layout({ locale, changeLocale }) {
       <NotificationContainer />
     </>
   );
+}
+
+renderLayout.propTypes = {
+  user: userPropType.isRequired,
+  renderGameMenu: PropTypes.func.isRequired,
+  renderLanguage: PropTypes.func.isRequired,
+  locale: PropTypes.string.isRequired,
+};
+
+function Layout({ locale, changeLocale }) {
+  const cachedUser = localStorage.getItem('user');
+  const initialUser = cachedUser ? JSON.parse(cachedUser) : {};
+  const [user, setUser] = useState(initialUser);
+
+  useEffect(() => {
+    updateUser(setUser);
+  }, [setUser]);
+
+  const logOut = useCallback(() => {
+    setUser({});
+    localStorage.removeItem('user');
+  }, [setUser]);
+
+  const renderGameMenu = React.useCallback(({ history }) => (
+    <GameMenu
+      logOut={logOut}
+      user={user}
+      history={history}
+    />
+  ), [logOut, user]);
+  const renderLanguage = useCallback(({ history }) => (
+    <LanguagePage
+      locale={locale}
+      changeLocale={changeLocale}
+      history={history}
+    />
+  ), [changeLocale, locale]);
+  return renderLayout({
+    user, renderGameMenu, renderLanguage, locale,
+  });
 }
 
 Layout.propTypes = {
--- src/components/Contributors/Contributors.jsx
+++ src/components/Contributors/Contributors.jsx
@@ -1,23 +1,38 @@
 import Octokat from 'octokat';
 import React from 'react';
+import PropTypes from 'prop-types';
 import Contributor from './Contributor';
+
+function mapContributors({ avatarUrl, htmlUrl, login }) {
+  return (
+    <Contributor
+      key={login}
+      avatarUrl={avatarUrl}
+      htmlUrl={htmlUrl}
+      login={login}
+    />
+  );
+}
+
+mapContributors.propTypes = {
+  login: PropTypes.string.isRequired,
+  htmlUrl: PropTypes.string.isRequired,
+  avatarUrl: PropTypes.string.isRequired,
+};
+
+function updateContributors(setContributors) {
+  const octo = new Octokat();
+  octo.repos('ricglz0201', 'ttt-react').contributors.fetch()
+    .then(({ items }) => {
+      const newContributors = items.map(mapContributors);
+      setContributors(newContributors);
+    });
+}
 
 export default function Contributors() {
   const [contributors, setContributors] = React.useState(null);
   React.useEffect(() => {
-    const octo = new Octokat();
-    octo.repos('ricglz0201', 'ttt-react').contributors.fetch()
-      .then(({ items }) => {
-        const newContributors = items.map(({ avatarUrl, htmlUrl, login }) => (
-          <Contributor
-            key={login}
-            avatarUrl={avatarUrl}
-            htmlUrl={htmlUrl}
-            login={login}
-          />
-        ));
-        setContributors(newContributors);
-      });
+    updateContributors(setContributors);
   }, [setContributors]);
   return (
     <div>
