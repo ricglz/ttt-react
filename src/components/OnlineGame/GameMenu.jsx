76b1901de33d0b93c9e20d63680becfb1ffb6148
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-07-13 17:30:40 -0700

    WIP

--- src/components/OnlineGame/GameMenu.jsx
+++ src/components/OnlineGame/GameMenu.jsx
@@ -6,122 +6,123 @@ import { gamesReference, boardReference } from '../../firebase/firebase';
 import { userPropType, historyProps } from '../../constants/props';
 import { fbInitialState, initialState } from '../../functions/HelperFunctions';
 
-class GameMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      games: { },
-      gameId: null,
-    };
-    this.joinGame = this.joinGame.bind(this);
-    this.hostNewGame = this.hostNewGame.bind(this);
-    this.surrender = this.surrender.bind(this);
-    this.handleLogOut = this.handleLogOut.bind(this);
-  }
+function GameMenu({ user, logOut, history }) {
+  const [state, setState] = React.useState({ games: {}, gameId: null });
+  const { games, gameId } = state;
+  const { uid, name } = user;
 
-  componentDidMount() {
-    const { user, history } = this.props;
+  React.useEffect(() => {
     if (Object.keys(user).length === 0) {
       history.push('/login');
       return;
     }
-    gamesReference().orderByChild('guestUid').equalTo(-1).on('value', (snapshot) => {
-      let games = snapshot.val();
-      games = games || { };
-      this.setState({ games });
-    }, (err) => {
-      NotificationManager.error(err.message);
-    })
-      .bind(this);
-  }
+    gamesReference().orderByChild('guestUid').equalTo(-1).on(
+      'value',
+      (snapshot) => {
+        let newGames = snapshot.val();
+        newGames = newGames || {};
+        setState({ ...state, games: newGames });
+      }, (err) => {
+        NotificationManager.error(err.message);
+      },
+    );
+  }, [user, history, setState, state]);
 
-  hostNewGame() {
-    const { uid, name } = this.props.user; // eslint-disable-line react/destructuring-assignment
+  const hostNewGame = React.useCallback(() => {
     const newGame = fbInitialState(uid, name);
     const ref = gamesReference().push(newGame);
-    this.setState({ gameId: ref.key });
-  }
+    setState({ ...state, gameId: ref.key });
+  }, [setState, state, name, uid]);
 
-  joinGame(gameId, hostUid) {
-    const { uid } = this.props.user; // eslint-disable-line react/destructuring-assignment
+  const joinGame = React.useCallback((selectedGameId, hostUid) => {
     if (hostUid !== uid) {
-      boardReference(gameId).update({ guestUid: uid });
+      boardReference(selectedGameId).update({ guestUid: uid });
     }
-    this.setState({ gameId });
-  }
+    setState({ ...state, gameId: selectedGameId });
+  }, [uid, setState, state]);
 
-  surrender({ guestUid, hostUid }) {
-    const { uid } = this.props.user; // eslint-disable-line react/destructuring-assignment
-    const { gameId } = this.state;
+  const surrender = React.useCallback(({ guestUid, hostUid }) => {
     if (uid === hostUid) {
       if (guestUid === -1) {
         boardReference(gameId).remove();
       } else {
-        const state = initialState();
-        state.hostUid = guestUid;
-        state.guestUid = -1;
-        state.nextPlayerUid = guestUid;
-        boardReference(gameId).update(state);
+        const newState = initialState();
+        newState.hostUid = guestUid;
+        newState.guestUid = -1;
+        newState.nextPlayerUid = guestUid;
+        boardReference(gameId).update(newState);
       }
     } else {
-      const state = initialState();
-      state.guestUid = -1;
-      state.nextPlayerUid = hostUid;
-      boardReference(gameId).update(state);
+      const newState = initialState();
+      newState.guestUid = -1;
+      newState.nextPlayerUid = hostUid;
+      boardReference(gameId).update(newState);
     }
-    this.setState({ gameId: null });
-  }
+    setState({ ...state, gameId: null });
+  }, [uid, gameId, setState, state]);
 
-  handleLogOut() {
-    const { logOut, history } = this.props;
+  const handleLogOut = React.useCallback(() => {
     logOut();
     history.push('/login');
-  }
+  }, [logOut, history]);
 
-  renderGames(games) {
-    return Object.keys(games).map((key) => {
+  const renderGames = React.useCallback(() => (
+    Object.keys(games).map((key) => {
       const { hostUid, hostName } = games[key];
       return (
-        <li className="list-group-item" key={key}>
-          <span>{hostName}</span>
-          <button
-            className="btn ml-5 btn-outline-dark"
-            type="button"
-            onClick={() => this.joinGame(key, hostUid)}
-          >
-            Join Game
-          </button>
-        </li>
+        <GameButton
+          key={key}
+          id={key}
+          joinGame={joinGame}
+          hostName={hostName}
+          hostUid={hostUid}
+        />
       );
-    });
-  }
+    })
+  ), [joinGame, games]);
 
-  render() {
-    const { games, gameId } = this.state;
-    const { uid } = this.props.user; // eslint-disable-line react/destructuring-assignment
-    return (
-      <React.Fragment>
-        { gameId ? (
-          <OnlineGame gameId={gameId} userId={uid} back={this.surrender} />
-        ) : (
-          <React.Fragment>
-            <div className="row justify-content-center">
-              <h1>Rooms</h1>
-            </div>
-            <hr />
-            <div className="row">
-              <Button text="Host new game" func={this.hostNewGame} />
-              <Button text="Log Out" func={this.handleLogOut} />
-            </div>
-            <ul className="list-group">
-              { this.renderGames(games) }
-            </ul>
-          </React.Fragment>
-        )}
-      </React.Fragment>
-    );
-  }
+  return (
+    <>
+      {gameId ? (
+        <OnlineGame gameId={gameId} userId={uid} back={surrender} />
+      ) : (
+        <>
+          <div className="row justify-content-center">
+            <h1>Rooms</h1>
+          </div>
+          <hr />
+          <div className="row">
+            <Button text="Host new game" func={hostNewGame} />
+            <Button text="Log Out" func={handleLogOut} />
+          </div>
+          <ul className="list-group">
+            {renderGames()}
+          </ul>
+        </>
+      )}
+    </>
+  );
 }
+
+const GameButton = ({
+  joinGame, hostUid, hostName, id,
+}) => {
+  const onClick = React.useCallback(() => {
+    joinGame(id, hostUid);
+  }, [joinGame, id, hostUid]);
+  return (
+    <li className="list-group-item">
+      <span>{hostName}</span>
+      <button
+        className="btn ml-5 btn-outline-dark"
+        type="button"
+        onClick={onClick}
+      >
+        Join Game
+      </button>
+    </li>
+  );
+};
 
 const Button = ({ text, func }) => (
   <div className="col text-center">
@@ -140,6 +141,13 @@ GameMenu.propTypes = {
 Button.propTypes = {
   text: PropTypes.string.isRequired,
   func: PropTypes.func.isRequired,
+};
+
+GameButton.propTypes = {
+  joinGame: PropTypes.func.isRequired,
+  hostUid: PropTypes.string.isRequired,
+  hostName: PropTypes.string.isRequired,
+  id: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
 };
 
 export default GameMenu;
