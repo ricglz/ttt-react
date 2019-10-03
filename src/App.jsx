2e85c7beb80eb6a91ab969c6fa669a7104eaff74
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-10-03 12:19:38 -0500

    Fix problem due to update in react-int version

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
 import React, { useState, useCallback } from 'react';
-import { addLocaleData, IntlProvider } from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import fr from 'react-intl/locale-data/fr';
-import es from 'react-intl/locale-data/es';
-import pt from 'react-intl/locale-data/pt';
-import ar from 'react-intl/locale-data/ar';
-import uk from 'react-intl/locale-data/uk';
-import id from 'react-intl/locale-data/id';
-import sr from 'react-intl/locale-data/sr';
-import da from 'react-intl/locale-data/da';
-import ko from 'react-intl/locale-data/ko';
-import ja from 'react-intl/locale-data/ja';
-import mr from 'react-intl/locale-data/mr';
-import hi from 'react-intl/locale-data/hi';
-import it from 'react-intl/locale-data/it';
-import de from 'react-intl/locale-data/de';
-import ru from 'react-intl/locale-data/ru';
-import sv from 'react-intl/locale-data/sv';
-import zh from 'react-intl/locale-data/zh';
-import cs from 'react-intl/locale-data/cs';
-import ca from 'react-intl/locale-data/ca';
-import tr from 'react-intl/locale-data/tr';
+import { IntlProvider } from 'react-intl';
 import Messages from './messages/Messages';
 import Layout from './Layout';
-
-addLocaleData(
-  [...en, ...fr, ...es, ...pt, ...it, ...hi, ...mr, ...ko, ...ja, ...da,
-    ...sr, ...id, ...uk, ...de, ...ru, ...sv, ...zh, ...ar, ...cs, ...tr,
-    ...ca],
-);
 
 function getDirection(locale) {
   return locale === 'ar' ? ['rtl', 'text-right'] : ['ltr', 'text-left'];
