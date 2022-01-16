f63a710598b6ce6dd7c8936ab16709fbf112bb34
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-01-16 12:58:52 -0600

    Translate LanguageButton component to a typescript file

--- src/components/Languages/LanguagePage.tsx
+++ src/components/Languages/LanguagePage.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import LanguageButton from '../Layout/LanguageButton';
+import LanguageButton from './LanguageButton';
 
 const LANGUAGES = [
   'en', 'es', 'fr', 'pt', 'de', 'it', 'hi', 'mr', 'ko', 'ja', 'da', 'sr',
   'id', 'uk', 'ru', 'sv', 'zh', 'ar', 'ca', 'cs', 'tr',
 ];
 
-interface LanguagePageProps {
+export interface LanguagePageProps {
   changeLocale: (locale: string) => void,
-  locale: string,
+  currentLocale: string,
 }
 
 const LanguagePage = (props: LanguagePageProps) => (
   <div className="row justify-content-center mt-5">
-    {LANGUAGES.map((lang) => <LanguageButton key={lang} {...props} locale={lang} />)}
+    {LANGUAGES.map((lang) => <LanguageButton key={lang} locale={lang} {...props} />)}
   </div>
 );
 
