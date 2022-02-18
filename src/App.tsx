1eea0cfe809b6852816e5aa85cee8b54f3fefb56
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-02-18 13:19:38 -0600

    Add test for Home view

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import React, { useState, useCallback } from 'react';
-import { IntlProvider } from 'react-intl';
-import messages from './messages';
+import React from 'react';
 import Layout from './Layout';
-
-function getDirection(locale: string) {
-  return locale === 'ar' ? ['rtl', 'text-right'] : ['ltr', 'text-left'];
-}
+import ContextsProvider from './ContextsProvider';
 
 function App() {
-  const [locale, setLocale] = useState('en');
-
-  const changeLocale = useCallback((newLocale) => {
-    setLocale(newLocale);
-  }, []);
-
-  const [direction, klass] = getDirection(locale);
-
-  return (
-    <IntlProvider locale={locale} messages={messages[locale]}>
-      <div dir={direction} className={klass}>
-        <Layout changeLocale={changeLocale} locale={locale} />
-      </div>
-    </IntlProvider>
-  );
+  return <ContextsProvider ChildrenComponent={Layout} />;
 }
 
 export default App;
