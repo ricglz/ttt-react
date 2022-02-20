8e7c8d937904ee1ecba61350c8f34b61e33666bc
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-02-20 14:28:48 -0600

    Add testing for Game

---
+++ src/components/Game/__tests__/Game.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import Home from '../../Home/Home';
+import Game from '../Game';
+import ContextsProvider from '../../../ContextsProvider';
+
+function Component() {
+  return (
+    <ContextsProvider
+      ChildrenComponent={Home}
+      RouterComponent={MemoryRouter}
+    >
+      <Game />
+    </ContextsProvider>
+  );
+}
+
+it('renders without crashing', () => {
+  const component = <Component />;
+  const tree = renderer.create(component).toJSON();
+  expect(tree).toMatchSnapshot();
+});
