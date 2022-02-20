a6a4755410b3022ebb688539243b62611ef4810d
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-02-20 15:35:51 -0600

    Create Cell tests

---
+++ src/components/Board/__tests__/Cell.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import type { BigBoard } from '../../../@types/general';
+import { Cell as CellEnum } from '../../../@types/general_enums';
+import { newBoard } from '../../../functions/HelperFunctions';
+import Cell from '../Cell';
+
+describe('Cell', () => {
+  const boardNum = 0;
+  const cellNum = 0;
+  let boardGame: BigBoard;
+
+  beforeEach(() => {
+    boardGame = newBoard();
+  });
+
+  test.each([CellEnum.X, CellEnum.O, CellEnum.NONE])('Value is %s', (value) => {
+    boardGame[boardNum][cellNum] = value;
+    const component = (
+      <Cell
+        boardNum={boardNum}
+        boardGame={boardGame}
+        cellNum={cellNum}
+        currentBoard={-1}
+        handleClick={() => {}}
+      />
+    );
+    const tree = renderer.create(component).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
