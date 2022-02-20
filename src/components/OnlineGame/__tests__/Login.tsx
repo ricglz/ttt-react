2aee31fe8fb92372768b63e5497524831eae85aa
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-02-20 16:47:39 -0600

    Create test for Login

---
+++ src/components/OnlineGame/__tests__/Login.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import Home from '../../Home/Home';
+import Login from '../Login';
+import ContextsProvider from '../../../ContextsProvider';
+
+function Component() {
+  return (
+    <ContextsProvider
+      ChildrenComponent={Home}
+      RouterComponent={MemoryRouter}
+    >
+      <Login />
+    </ContextsProvider>
+  );
+}
+
+it('renders without crashing', () => {
+  const component = <Component />;
+  const tree = renderer.create(component).toJSON();
+  expect(tree).toMatchSnapshot();
+});
