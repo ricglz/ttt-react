e6732824834809c7d5f42741c389565c8e0e93b2
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-03-05 10:49:54 -0600

    Remove import react in GameHooks.ts

--- src/functions/GameHooks.ts
+++ src/functions/GameHooks.ts
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useCallback, useState } from 'react';
+
 import {
   isOccupied,
   theresAWinner,
@@ -10,37 +11,44 @@ import {
 import { Cell } from '../@types/general_enums';
 
 import type {
-  BigBoard, Board, Game, GeneralBoardIndex,
+  BigBoard,
+  Board,
+  Game,
+  GeneralBoardIndex,
 } from '../@types/general';
 import type { Option } from '../components/Game/DifficultySelect';
 import type { Player } from '../@types/general_enums';
-import Ai from './Ai';
+import type Ai from './Ai';
 
 type SetGameFn = (game: Game) => void;
 
 // TODO: Check to modify all the hooks in this file to be a reducer
 export function useGameHooks(game: Game, setGame: SetGameFn, ai: Ai) {
-  const {
-    boardGame, currentBoard, currentPlayer,
-  } = game;
+  const { boardGame, currentBoard, currentPlayer } = game;
 
-  const canClick = React.useCallback((board, id) => {
-    const currentValue = boardGame[board][id];
-    if (isOccupied(currentValue)) return false;
-    return board === currentBoard || currentBoard === -1;
-  }, [boardGame, currentBoard]);
+  const canClick = useCallback(
+    (board, id) => {
+      const currentValue = boardGame[board][id];
+      if (isOccupied(currentValue)) return false;
+      return board === currentBoard || currentBoard === -1;
+    },
+    [boardGame, currentBoard],
+  );
 
-  const pvpMove = React.useCallback((boardCopy, newMoveNumber, id) => {
-    setGame({
-      ...game,
-      boardGame: boardCopy,
-      moveNumber: newMoveNumber,
-      currentBoard: id,
-      currentPlayer: getNextPlayer(currentPlayer),
-    });
-  }, [setGame, game, currentPlayer]);
+  const pvpMove = useCallback(
+    (boardCopy, newMoveNumber, id) => {
+      setGame({
+        ...game,
+        boardGame: boardCopy,
+        moveNumber: newMoveNumber,
+        currentBoard: id,
+        currentPlayer: getNextPlayer(currentPlayer),
+      });
+    },
+    [setGame, game, currentPlayer],
+  );
 
-  const newGame = React.useCallback(() => {
+  const newGame = useCallback(() => {
     ai.cleanVariables();
     setGame({ ...game, ...initialState() });
   }, [setGame, game]);
@@ -49,14 +57,14 @@ export function useGameHooks(game: Game, setGame: SetGameFn, ai: Ai) {
 }
 
 type AIHooksProps = {
-  moveNumber: number,
-  selectedOption: Option | null,
-  setGame: SetGameFn,
-  changeScore: (winner: Cell) => void,
-  newGame: () => void,
-  setSelectedOption: (option: Option) => void,
-  game: Game,
-  ai: Ai
+  moveNumber: number;
+  selectedOption: Option | null;
+  setGame: SetGameFn;
+  changeScore: (winner: Cell) => void;
+  newGame: () => void;
+  setSelectedOption: (option: Option) => void;
+  game: Game;
+  ai: Ai;
 };
 
 export function useAIHooks({
@@ -69,133 +77,174 @@ export function useAIHooks({
   game,
   ai,
 }: AIHooksProps) {
-  const aiMove = React.useCallback((boardCopy, id, newMoveNumber) => {
-    const board = [...boardCopy] as BigBoard;
-    const currentDifficulty = selectedOption === null ? 1 : selectedOption.value;
-    const move = ai.makeMove({ board: board[id], currentBoard: id, currentDifficulty });
-    board[id][move] = -1;
-    const winner = theresAWinner(board[id]);
-    if (winner) {
-      alertWinner(winner);
-      changeScore(winner);
-      newGame();
-    } else {
-      ai.aiMadeAMove(board[id]);
-      setGame({
-        ...game,
-        boardGame: board,
-        moveNumber: newMoveNumber + 1,
-        currentBoard: move,
+  const aiMove = useCallback(
+    (boardCopy, id, newMoveNumber) => {
+      const board = [...boardCopy] as BigBoard;
+      const currentDifficulty = selectedOption === null ? 1 : selectedOption.value;
+      const move = ai.makeMove({
+        board: board[id],
+        currentBoard: id,
+        currentDifficulty,
       });
-    }
-  }, [changeScore, newGame, selectedOption, setGame, game]);
+      board[id][move] = -1;
+      const winner = theresAWinner(board[id]);
+      if (winner) {
+        alertWinner(winner);
+        changeScore(winner);
+        newGame();
+      } else {
+        ai.aiMadeAMove(board[id]);
+        setGame({
+          ...game,
+          boardGame: board,
+          moveNumber: newMoveNumber + 1,
+          currentBoard: move,
+        });
+      }
+    },
+    [changeScore, newGame, selectedOption, setGame, game],
+  );
 
-  const handleChange = React.useCallback((newSelectedOption) => {
-    setSelectedOption(newSelectedOption);
-    if (moveNumber >= 0) {
-      newGame();
-    }
-  }, [setSelectedOption, moveNumber, newGame]);
+  const handleChange = useCallback(
+    (newSelectedOption) => {
+      setSelectedOption(newSelectedOption);
+      if (moveNumber >= 0) {
+        newGame();
+      }
+    },
+    [setSelectedOption, moveNumber, newGame],
+  );
 
   return { aiMove, handleChange };
 }
 
 export function useScore() {
-  const [oWins, setOWins] = React.useState(0);
-  const [xWins, setXWins] = React.useState(0);
-  const changeScore = React.useCallback((value: Cell) => {
-    if (value === Cell.O) {
-      setOWins(oWins + 1);
-    } else {
-      setXWins(xWins + 1);
-    }
-  }, [setOWins, setXWins, oWins, xWins]);
+  const [oWins, setOWins] = useState(0);
+  const [xWins, setXWins] = useState(0);
+  const changeScore = useCallback(
+    (value: Cell) => {
+      if (value === Cell.O) {
+        setOWins(oWins + 1);
+      } else {
+        setXWins(xWins + 1);
+      }
+    },
+    [setOWins, setXWins, oWins, xWins],
+  );
   return { oWins, xWins, changeScore };
 }
 
 interface AfterMoveOnlineProps {
-  changeScore: (winner: Cell) => void,
-  newGame: () => void,
-  pvpMove: (board: BigBoard, newMoveNumber: number, id: number) => void,
+  changeScore: (winner: Cell) => void;
+  newGame: () => void;
+  pvpMove: (board: BigBoard, newMoveNumber: number, id: number) => void;
 }
 
 type AfterMoveCallbackProps = {
-  winner: Cell | null,
-  newMoveNumber: number,
-  board: GeneralBoardIndex,
-  boardCopy: BigBoard,
-  id: number,
+  winner: Cell | null;
+  newMoveNumber: number;
+  board: GeneralBoardIndex;
+  boardCopy: BigBoard;
+  id: number;
 };
 
 export function useAfterMoveOnline({
-  changeScore, newGame, pvpMove,
+  changeScore,
+  newGame,
+  pvpMove,
 }: AfterMoveOnlineProps) {
-  const afterMove = React.useCallback(({
-    winner, newMoveNumber, boardCopy, id,
-  }: AfterMoveCallbackProps) => {
-    if (winner) {
-      alertWinner(winner);
-      changeScore(winner);
-      newGame();
-    } else if (newMoveNumber === 81) {
-      newGame();
-    } else {
-      pvpMove(boardCopy, newMoveNumber, id);
-    }
-  }, [changeScore, newGame, pvpMove]);
+  const afterMove = useCallback(
+    ({
+      winner, newMoveNumber, boardCopy, id,
+    }: AfterMoveCallbackProps) => {
+      if (winner) {
+        alertWinner(winner);
+        changeScore(winner);
+        newGame();
+      } else if (newMoveNumber === 81) {
+        newGame();
+      } else {
+        pvpMove(boardCopy, newMoveNumber, id);
+      }
+    },
+    [changeScore, newGame, pvpMove],
+  );
   return afterMove;
 }
 
 interface AfterMoveProps extends AfterMoveOnlineProps {
-  ai: Ai,
-  aiMove: (board: BigBoard, id: number, newMoveNumber: number) => void,
-  isAi: boolean,
+  ai: Ai;
+  aiMove: (board: BigBoard, id: number, newMoveNumber: number) => void;
+  isAi: boolean;
 }
 
 export function useAfterMove({
-  ai, aiMove, changeScore, newGame, pvpMove, isAi,
+  ai,
+  aiMove,
+  changeScore,
+  newGame,
+  pvpMove,
+  isAi,
 }: AfterMoveProps) {
-  const afterMove = React.useCallback(({
-    winner, newMoveNumber, board, boardCopy, id,
-  }: AfterMoveCallbackProps) => {
-    if (winner) {
-      alertWinner(winner);
-      changeScore(winner);
-      newGame();
-    } else if (newMoveNumber === 81) {
-      newGame();
-    } else if (isAi) {
-      ai.playerMadeAMove(board, boardCopy[board]);
-      aiMove(boardCopy, id, newMoveNumber);
-    } else {
-      pvpMove(boardCopy, newMoveNumber, id);
-    }
-  }, [ai, aiMove, changeScore, newGame, pvpMove]);
+  const afterMove = useCallback(
+    ({
+      winner,
+      newMoveNumber,
+      board,
+      boardCopy,
+      id,
+    }: AfterMoveCallbackProps) => {
+      if (winner) {
+        alertWinner(winner);
+        changeScore(winner);
+        newGame();
+      } else if (newMoveNumber === 81) {
+        newGame();
+      } else if (isAi) {
+        ai.playerMadeAMove(board, boardCopy[board]);
+        aiMove(boardCopy, id, newMoveNumber);
+      } else {
+        pvpMove(boardCopy, newMoveNumber, id);
+      }
+    },
+    [ai, aiMove, changeScore, newGame, pvpMove],
+  );
   return afterMove;
 }
 
 type HandleClickProps = {
-  canClick: (board: Board, id: number) => boolean,
-  boardGame: BigBoard,
-  moveNumber: number,
-  currentPlayer: Player,
-  afterMove: (args: AfterMoveCallbackProps) => void,
+  canClick: (board: Board, id: number) => boolean;
+  boardGame: BigBoard;
+  moveNumber: number;
+  currentPlayer: Player;
+  afterMove: (args: AfterMoveCallbackProps) => void;
 };
 
 export function useHandleClick({
-  canClick, boardGame, moveNumber, currentPlayer, afterMove,
+  canClick,
+  boardGame,
+  moveNumber,
+  currentPlayer,
+  afterMove,
 }: HandleClickProps) {
-  const handleSquareClick = React.useCallback((board, id) => {
-    if (!canClick(board, id)) {
-      return;
-    }
-    const boardCopy: BigBoard = [...boardGame];
-    const newMoveNumber = moveNumber + 1;
-    boardCopy[board][id] = getPlayerCellValue(currentPlayer);
-    const winner = theresAWinner(boardCopy[board]);
-    afterMove({
-      winner, newMoveNumber, board, boardCopy, id,
-    });
-  }, [canClick, boardGame, moveNumber, currentPlayer, afterMove]);
+  const handleSquareClick = useCallback(
+    (board, id) => {
+      if (!canClick(board, id)) {
+        return;
+      }
+      const boardCopy: BigBoard = [...boardGame];
+      const newMoveNumber = moveNumber + 1;
+      boardCopy[board][id] = getPlayerCellValue(currentPlayer);
+      const winner = theresAWinner(boardCopy[board]);
+      afterMove({
+        winner,
+        newMoveNumber,
+        board,
+        boardCopy,
+        id,
+      });
+    },
+    [canClick, boardGame, moveNumber, currentPlayer, afterMove],
+  );
   return handleSquareClick;
 }
