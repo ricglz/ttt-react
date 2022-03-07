8f4f00206dae2fa817d1e7d3d0b42afcff1485b4
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-03-06 19:39:40 -0600

    Working better but problem with svg

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -24,7 +24,10 @@ export default function register() {
     "serviceWorker" in navigator
   ) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(import.meta.env.PUBLIC_URL, window.location);
+    const publicUrl = new URL(
+      String(import.meta.env.PUBLIC_URL ?? ""),
+      window.location as unknown as URL
+    );
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -55,12 +58,13 @@ export default function register() {
   }
 }
 
-function registerValidSW(swUrl) {
+function registerValidSW(swUrl: string | URL) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
+        if (installingWorker === null) return;
         installingWorker.onstatechange = () => {
           if (installingWorker.state === "installed") {
             if (navigator.serviceWorker.controller) {
@@ -84,14 +88,14 @@ function registerValidSW(swUrl) {
     });
 }
 
-function checkValidServiceWorker(swUrl) {
+function checkValidServiceWorker(swUrl: string) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
     .then((response) => {
       // Ensure service worker exists, and that we really are getting a JS file.
       if (
         response.status === 404 ||
-        response.headers.get("content-type").indexOf("javascript") === -1
+        response.headers.get("content-type")?.indexOf("javascript") === -1
       ) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then((registration) => {
