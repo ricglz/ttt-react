6058a21d5e8b8b0198c2970ebc773e1410b01253
Ricardo J González C <26823011+ricglz@users.noreply.github.com>
2022-03-06 19:58:13 -0600

    Fix bug with svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svg from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), svg()],
+  build: {sourcemap: true}
 })
